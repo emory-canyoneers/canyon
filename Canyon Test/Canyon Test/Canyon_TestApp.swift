0f4c590298d581eb13fc7caf1c17f2be95e15f35
riyanshah34 <122489730+riyanshah34@users.noreply.github.com>
2024-03-06 15:18:16 -0500

    Add files via upload
    Swift Extension

---
+++ Canyon Test/Canyon Test/Canyon_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Canyon_TestApp.swift
+//  Canyon Test
+//
+//  Created by Riyan Shah on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Canyon_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
