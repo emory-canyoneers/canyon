3a14278ca6d1ac70a56c3634eeb7ca310c9f4515
Thalia Papageorgiou <tpapage@emory.edu>
2024-04-22 22:39:39 -0400

    working question details modal

--- frontend/src/components/Question.js
+++ frontend/src/components/Question.js
@@ -1,15 +1,19 @@
 import { useState } from 'react';
-import { Modal, Text, TouchableOpacity, View } from 'react-native';
+import { Modal, Pressable, Text, TouchableOpacity, View } from 'react-native';
 import { styles } from '../styles/Group';
 
 export default function Question({question}) {
     const [open, setOpen] = useState(false);
     const [selectedQuestion, setSelectedQuestion] = useState(null);
+    console.log(question);
 
-    const openDetails = (question) => {
-        console.log("Opening details for question", question);
+    const openDetails = (question) => { 
         setSelectedQuestion(question);
         setOpen(true); //opens the second modal 
+    }
+
+    const closeModal = () => {
+        setOpen(false);
     }
 
     return (
@@ -23,12 +27,21 @@ export default function Question({question}) {
                 visible={open}
                 onRequestClose={() => setOpen(false)}
             >
-                <View style={styles.centeredView}>
-                    <View style={styles.modalView}>
-                        <Text style={{color:'white', fontWeight:'bold'}}>WORKING</Text>
-                        <Text style={styles.modalText}>{selectedQuestion?.question}</Text>
+                <Pressable
+                    style={[styles.centeredView]}
+                    onPress={() => setOpen(false)}
+                >
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                                <Text style={{color:'white', fontWeight:'bold'}}>{question.question}</Text>
+                                {question.responses.map((response) => (
+                                    <View key={response.id}>
+                                        <Text style={{color:'white'}}>{response.user.name}: {response.response}</Text>
+                                    </View>
+                                ))}
+                        </View>
                     </View>
-                </View>
+                </Pressable>
             </Modal>
         </View>
     );
