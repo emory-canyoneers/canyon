fc3efed26e58e508e6bf58a6c217f6dffe2b4152
andrewtlu <andrew.lu@emory.edu>
2024-04-25 23:42:45 -0400

    Fix edit and respond, update state properly

--- frontend/src/screens/AnswerPage.js
+++ frontend/src/screens/AnswerPage.js
@@ -8,31 +8,28 @@ import {
   View,
 } from "react-native";
 import { AuthContext } from "../store/auth";
-import {
-  InfoContext,
-  getAllQuestions,
-  getGroup,
-  getResponseId,
-} from "../store/info";
+import { InfoContext, getAllQuestions, getResponseId, getGroup, getCurrentQuestion } from "../store/info";
 import { SelfContext } from "../store/self";
 import { styles } from "../styles/Answer";
 import { colors } from "../styles/colors";
 
 export default AnswerPage = () => {
-  const token = useContext(AuthContext)[0];
-  const groups = useContext(InfoContext)[0];
-  const selfId = useContext(SelfContext)[0].id;
-  const [unanswered, setUnanswered] = useState([]);
-  const [answered, setAnswered] = useState([]);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [currentQuestion, setCurrentQuestion] = useState({});
-  const [answer, setAnswer] = useState("");
+    const token = useContext(AuthContext)[0];
+    const groups = useContext(InfoContext)[0];
+    const setGroups = useContext(InfoContext)[1];
+    const selfId = useContext(SelfContext)[0].id;
+    const [unanswered, setUnanswered] = useState([]);
+    const [answered, setAnswered] = useState([]);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [currentQuestion, setCurrentQuestion] = useState({});
 
-  useEffect(() => {
-    const questions = getAllQuestions(groups, selfId);
-    setAnswered(questions.answered);
-    setUnanswered(questions.unanswered);
-  }, []);
+    const [answer, setAnswer] = useState("");
+
+    useEffect(() => {
+        const questions = getAllQuestions(groups, selfId);
+        setAnswered(questions.answered);
+        setUnanswered(questions.unanswered);
+    }, [groups]);
 
   const answerQuestion = (question) => {
     setCurrentQuestion(question);
@@ -40,148 +37,143 @@ export default AnswerPage = () => {
     setModalVisible(true);
   };
 
-  const editQuestion = (question) => {
-    setCurrentQuestion(question);
-    setAnswer(question.answer || "");
-    setModalVisible(true);
-  };
-
-  const getResponse = (question) => {
-    return question.responses.find((response) => response.user.id === selfId);
-  };
-
-  const submitAnswer = async () => {
-    const url = `http://joincanyon.org/responses`;
-    const data = {
-      groupId: currentQuestion.groupId,
-      response: answer,
-    };
-    const options = {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + token,
-      },
-      body: JSON.stringify(data),
+    const editQuestion = (question) => {
+        setCurrentQuestion(question);
+        setAnswer(question.answer);
+        setModalVisible(true);
     };
 
-    console.log(data);
-
-    try {
-      const response = await fetch(url, options)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! ${response.status}`);
-          } else {
-            currentQuestion.isAnswered = true;
-            console.log("Successfully submitted/ edited answer!");
-
-            return response.json();
-          }
-        })
-        .then((data) => {
-          return data;
-        });
-    } catch (error) {
-      console.error("Failed to submit answer: ", error);
-    } finally {
-      setModalVisible(false);
+    const getResponse = (question) => {
+        if (question && question.responses) {
+            return question.responses.find(response => response.user.id === selfId);
+        }
+        return null;
     }
-  };
 
-  const editAnswer = async () => {
-    if (!currentQuestion.id) {
-      console.error("No response ID found for question: ", currentQuestion);
-      return;
-    }
-    const url = `http://joincanyon.org/responses`;
-    const data = {
-      responseId: getResponseId(answered, selfId, currentQuestion.id),
-      response: answer,
+    const submitAnswer = async () => {
+        const url = `http://joincanyon.org/responses`;
+        const data = {
+            groupId: currentQuestion.group,
+            response: answer
+        };
+        const options = {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + token,
+            },
+            body: JSON.stringify(data)
+        };
+
+        try{
+            const response = await fetch(url, options)
+                .then(response => {
+                    if(!response.ok){
+                        throw new Error(`HTTP error! ${response.status}`);
+                    } else {
+                        return response.json()
+                    }
+                })
+                .then(data => {
+                    return data;
+                });
+
+            const group = getGroup(groups, response.group);
+            getCurrentQuestion(group).responses.push(response);
+            setGroups([...groups]);
+        } catch (error) {
+            console.error("Failed to submit answer: ", error);
+        } finally {
+            setModalVisible(false);
+        }
     };
-    const options = {
-      method: "PUT",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + token,
-      },
-      body: JSON.stringify(data),
+
+    const editAnswer = async () => {
+        const url = `http://joincanyon.org/responses`;
+        console.log(getResponseId(selfId, currentQuestion));
+        const data = {
+            responseId: getResponseId(selfId, currentQuestion),
+            response: answer
+        };
+        const options = {
+            method: "PUT",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + token,
+            },
+            body: JSON.stringify(data)
+        };
+
+        try{
+            const response = await fetch(url, options)
+                .then(response => {
+                    if(!response.ok){
+                        throw new Error(`HTTP error! ${response.status}`);
+                    } else {            
+                        return response.json()
+                    }
+                })
+                .then(data => {
+                    return data;
+                });
+            
+            console.log(getResponseId(selfId, currentQuestion))
+            console.log(JSON.stringify(response, null, 2))
+
+
+            const group = getGroup(groups, response.group);
+            // get the response from group most recent issue and replace
+            getCurrentQuestion(group).responses.find(response => response.user.id === selfId).response = response.response;
+            setGroups([...groups]);
+        } catch (error) {
+            console.error("Failed to submit answer: ", error);
+        } finally {
+            setModalVisible(false);
+        }
     };
-    console.log(currentQuestion.id);
-    console.log(data);
-
-    try {
-      const response = await fetch(url, options)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! ${response.status}`);
-          } else {
-            currentQuestion.isAnswered = true;
-            console.log("Successfully submitted/ edited answer!");
-
-            return response.json();
-          }
-        })
-        .then((data) => {
-          return data;
-        });
-    } catch (error) {
-      console.error("Failed to submit answer: ", error);
-    } finally {
-      setModalVisible(false);
-    }
-  };
-
-  return (
-    <ScrollView
-      style={styles.scrollView}
-      contentContainerStyle={styles.container}
-    >
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}
-      >
-        <Pressable
-          style={styles.centeredView}
-          onPress={() => setModalVisible(false)}
-        >
-          <View style={styles.centeredView}>
-            <View
-              style={styles.modalView}
-              onStartShouldSetResponder={() => true}
-            >
-              <Text style={styles.questionInModal}>
-                {currentQuestion.question}
-              </Text>
-              <TextInput
-                style={styles.answer_input}
-                onChangeText={setAnswer}
-                value={answer}
-                multiline={true}
-                placeholder="Your answer here..."
-                placeholderTextColor="white"
-              />
-              <Pressable
-                style={[styles.button, styles.buttonClose]}
-                onPress={currentQuestion.isAnswered ? editAnswer : submitAnswer}
-              >
-                <Text style={styles.textStyle}>
-                  {currentQuestion.isAnswered ? "Edit" : "Submit"}
-                </Text>
-              </Pressable>
-            </View>
-          </View>
-        </Pressable>
-      </Modal>
-
-      <View style={styles.body}>
-        {/* Answer questions */}
-        <Text style={styles.title}>Your groups are waiting to hear back!</Text>
-        <Text style={styles.heading}>Unanswered questions:</Text>
+    
+    return (
+        <ScrollView style={styles.scrollView} contentContainerStyle={styles.container}>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => setModalVisible(false)}
+                >
+                <Pressable 
+                    style = {styles.centeredView}
+                    onPress = {() => setModalVisible(false)}
+                >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView} onStartShouldSetResponder={() => true}>
+                        <Text style={styles.questionInModal}>{currentQuestion.question}</Text>
+                        <TextInput
+                            style={styles.answer_input}
+                            onChangeText={setAnswer}
+                            value={answer}
+                            multiline={true}
+                            placeholder="Your answer here..."
+                            placeholderTextColor="white"
+                        />
+                        <Pressable
+                            style={[styles.button, styles.buttonClose]}
+                            onPress={getResponse(currentQuestion) ? editAnswer : submitAnswer}
+                        >
+                            <Text style={styles.textStyle}>
+                                {getResponse(currentQuestion) ? "Edit" : "Submit"}
+                            </Text>
+                        </Pressable>
+                    </View>
+                </View>
+                </Pressable>
+            </Modal>
+            
+            <View style={styles.body}>
+                {/* Answer questions */}
+                <Text style={styles.title}>Your groups are waiting to hear back!</Text>
+                <Text style={styles.heading}>Unanswered questions:</Text>
 
         <View style={styles.content}>
           {unanswered.map((question) => (
