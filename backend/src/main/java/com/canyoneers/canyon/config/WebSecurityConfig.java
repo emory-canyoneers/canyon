f5ad558eb55570d31776b4e8635a155ab8e31499
andrewtlu <andrew.lu@emory.edu>
2024-04-30 21:26:50 -0400

    More backend changes

--- backend/src/main/java/com/canyoneers/canyon/config/WebSecurityConfig.java
+++ backend/src/main/java/com/canyoneers/canyon/config/WebSecurityConfig.java
@@ -15,17 +15,20 @@ public class WebSecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         System.out.println("Connecting security filter chain");
         http
-            // this was the issue, csrf was blocking the /auth endpoint
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(
-                requests -> requests
-                    .requestMatchers("/auth").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/users").permitAll()
-                    .requestMatchers("/", "index.html", "AboutUs.html", "HowTo.html", "Group 9.svg", "nav.html", "logo1.png", "Vector.png", "/HeadShots/**", "/scripts/**", "/styles/**").permitAll()
-                    .anyRequest().authenticated());
-                    
+                // this was the issue, csrf was blocking the /auth endpoint
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(
+                        requests -> requests
+                                .requestMatchers("/auth").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                                .requestMatchers("/", "index.html", "AboutUs.html", "HowTo.html", "Group 9.svg",
+                                        "nav.html", "logo1.png", "Vector.png", "/static/**", "/HeadShots/**",
+                                        "/scripts/**", "/styles/**", "GetStarted.html", "expo-code.png")
+                                .permitAll()
+                                .anyRequest().authenticated());
+
         http
-            .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();
     }
